95bfb592e2465d0d59093e2643a32562076d7cde
Khiem Nhuong Joey Nguyen <knjoeyn@gmail.com>
2019-05-13 08:55:57 -0700

    Add constant file Add authentication to DataProvider

--- app/src/providers/DataProvider.js
+++ app/src/providers/DataProvider.js
@@ -6,35 +6,48 @@ import { HttpLink } from 'apollo-link-http';
 import { split } from 'apollo-link';
 import { getMainDefinition } from 'apollo-utilities';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
+import { AUTH_TOKEN } from '../constants';
 
 // TODO: via env configs
 const WS_URL = 'ws://localhost:4000';
 const HTTP_URL = 'http://localhost:4000';
 
+const authLink = setContext((_, { headers }) => {
+	const token = localStorage.getItem(AUTH_TOKEN);
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : ''
+		}
+	};
+});
+
 const wsLink = new WebSocketLink({
-  uri: WS_URL,
-  options: {
-    reconnect: true
-  }
+	uri: WS_URL,
+	options: {
+		reconnect: true,
+		connectionParams: {
+			authToken: localStorage.getItem(AUTH_TOKEN)
+		}
+	}
 });
 const httpLink = new HttpLink({
-  uri: HTTP_URL
+	uri: HTTP_URL
 });
 
 const link = split(
-  ({ query }) => {
-    const { kind, operation } = getMainDefinition(query);
-    return kind === 'OperationDefinition' && operation === 'subscription';
-  },
-  wsLink,
-  httpLink
+	({ query }) => {
+		const { kind, operation } = getMainDefinition(query);
+		return kind === 'OperationDefinition' && operation === 'subscription';
+	},
+	wsLink,
+	authLink.concat(httpLink)
 );
 
 const client = new ApolloClient({
-  link,
-  cache: new InMemoryCache()
+	link,
+	cache: new InMemoryCache()
 });
 
-export default ({ children }) => (
-  <ApolloProvider client={client}>{children}</ApolloProvider>
-);
+export default ({ children }) => <ApolloProvider client={client}>{children}</ApolloProvider>;
